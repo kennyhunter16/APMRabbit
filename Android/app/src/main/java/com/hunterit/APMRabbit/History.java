6228781ee65f85c5f41fa1a8942de0d4a612504a
HunterIT <kenneth@hunterit.ca>
2015-04-06 12:06:47 -0400

    added docs

--- Android/app/src/main/java/com/hunterit/APMRabbit/History.java
+++ Android/app/src/main/java/com/hunterit/APMRabbit/History.java
@@ -1,5 +1,7 @@
 package com.hunterit.APMRabbit;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,8 +12,28 @@ public class History extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.fragment_history, container, false);
+
+        SQLiteDatabase db = MySQLiteHelper.getReadableDatabase();
+
+        String[] projection = {
+                FeedEntry._ID,
+                FeedEntry.COLUMN_NAME_TITLE,
+        };
+
+// How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                FeedEntry.COLUMN_NAME_UPDATED + " DESC";
+
+        Cursor c = db.query(
+                FeedEntry.TABLE_NAME,  // The table to query
+                projection,                               // The columns to return
+                selection,                                // The columns for the WHERE clause
+                selectionArgs,                            // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
 
         return rootView;
     }
